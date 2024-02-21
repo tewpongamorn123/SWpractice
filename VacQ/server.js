09f15ccbefb8adc944aaf6f451f6aa9069493336
pongamorn jittamettakul <tewpongamorn@gmail.com>
2024-02-21 16:58:03 +0700

    Finish

--- VacQ/server.js
+++ VacQ/server.js
@@ -5,11 +5,8 @@ dotenv.config({path:'./config/config.env'})
 
 const app = express();
 
-app.get('/',(req,res) => {
-    //res.send('<h1>Hello from express<h1>');
-    res.json({name:'Brad'});
-    //res.status(200).json({success:true, data:{id:1}});
-});
+const hospitals = require('./routes/hospitals');
+app.use('/api/v1/hospitals',hospitals)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, 'mode on port ', PORT));
